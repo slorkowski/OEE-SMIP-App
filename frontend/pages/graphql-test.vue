c47b5f6e52f8c8cd221e0c61429190df9110c190
Scott Fasone <scott181182@yahoo.com>
2024-05-04 15:28:34 -0400

    docs(frontend): added some instruction to the graphql test page

--- frontend/pages/graphql-test.vue
+++ frontend/pages/graphql-test.vue
@@ -20,19 +20,40 @@ onBeforeMount(() => {
 </script>
 
 <template>
-    <v-list v-if="eqRef">
-        <v-list-item
-            v-for="et in eqRef.data"
-            :key="et.id"
-            :title="et.displayName ?? 'Unknown Type'"
-        >
-            <v-list-item-subtitle>
-                <v-chip
-                    v-for="oee_metric in et.childEquipment"
-                    :key="oee_metric.id"
-                    :text="oee_metric.displayName || 'unknown metric'"
-                />
-            </v-list-item-subtitle>
-        </v-list-item>
-    </v-list>
+    <section class="ma-4">
+        <h1>GraphQL Test Page</h1>
+        <p>
+            In order for this page to work, you'll have to request a header token from your GraphQL server and set an environment variable to it.
+        </p>
+        <p>
+            Specifically, if using the CESMII East instance, go to <a href="https://east.cesmii.net/">https://east.cesmii.net/</a>, open the dropdown for "Developer" and select "Request Header Token".
+        </p>
+        <p>
+            Then create a <code>.env</code> file in the <code>frontend</code> directory and add the following line:
+        </p>
+        <pre class="mt-4">
+    VITE_APP_GRAPHQL_TOKEN=&lt;token value, excluding "Bearer"&gt;
+        </pre>
+        <p>
+            This token expires relatively quickly, so you might have to update this if you're testing this page for a bit.
+        </p>
+        <p>
+            You should then see the content below:
+        </p>
+        <v-list v-if="eqRef" class="border-lg mt-4">
+            <v-list-item
+                v-for="et in eqRef.data"
+                :key="et.id"
+                :title="et.displayName ?? 'Unknown Type'"
+            >
+                <v-list-item-subtitle>
+                    <v-chip
+                        v-for="oee_metric in et.childEquipment"
+                        :key="oee_metric.id"
+                        :text="oee_metric.displayName || 'unknown metric'"
+                    />
+                </v-list-item-subtitle>
+            </v-list-item>
+        </v-list>
+    </section>
 </template>
