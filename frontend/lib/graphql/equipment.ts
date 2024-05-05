bf80278510f5004dfbd21b8cc828ebfe8372f35f
Scott Fasone <scott181182@yahoo.com>
2024-05-05 19:51:26 -0400

    style: changed indent size to 2

--- frontend/lib/graphql/equipment.ts
+++ frontend/lib/graphql/equipment.ts
@@ -6,32 +6,32 @@ import { GetEquipmentsDocument, GetOeeEquipmentTypesWithEquipmentIdsDocument  } 
 
 
 export interface EquipmentWithOEEHook {
-    data: EquipmentWithOeeFragment[] | null | undefined;
+  data: EquipmentWithOeeFragment[] | null | undefined;
 }
 
 export function useEquipmentWithOEE(): Ref<EquipmentWithOEEHook> {
-    const { result: typeRes } = useQuery(
-        GetOeeEquipmentTypesWithEquipmentIdsDocument,
-        {},
-        { errorPolicy: "ignore" },
-    );
+  const { result: typeRes } = useQuery(
+    GetOeeEquipmentTypesWithEquipmentIdsDocument,
+    {},
+    { errorPolicy: "ignore" },
+  );
 
-    const equipmentIds = computed(() => unique(
-        typeRes.value?.equipmentTypes?.flatMap((et) =>
-            et.equipments.map((eq) => eq.partOfId),
-        ).filter(isNonNullish) || [],
-    ));
+  const equipmentIds = computed(() => unique(
+    typeRes.value?.equipmentTypes?.flatMap((et) =>
+      et.equipments.map((eq) => eq.partOfId),
+    ).filter(isNonNullish) || [],
+  ));
 
-    const { result: eqRes } = useQuery(GetEquipmentsDocument, {
-        filter: {
-            id: { in: equipmentIds.value ?? [] },
-        },
-    }, {
-        enabled: equipmentIds.value && equipmentIds.value.length > 0,
-        errorPolicy: "ignore",
-    });
+  const { result: eqRes } = useQuery(GetEquipmentsDocument, {
+    filter: {
+      id: { in: equipmentIds.value ?? [] },
+    },
+  }, {
+    enabled: equipmentIds.value && equipmentIds.value.length > 0,
+    errorPolicy: "ignore",
+  });
 
-    return computed<EquipmentWithOEEHook>(() => ({
-        data: eqRes.value?.equipments,
-    }));
+  return computed<EquipmentWithOEEHook>(() => ({
+    data: eqRes.value?.equipments,
+  }));
 }
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,41 +4,41 @@ import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-    build: {
-        transpile: [ "vuetify" ],
+  devtools: { enabled: true },
+  build: {
+    transpile: [ "vuetify" ],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        config.plugins!.push(vuetify({ autoImport: true }));
+      });
     },
-    modules: [
-        (_options, nuxt) => {
-            nuxt.hooks.hook("vite:extendConfig", (config) => {
-                config.plugins!.push(vuetify({ autoImport: true }));
-            });
-        },
-        "@nuxt/eslint",
-        "@nuxtjs/apollo",
-    ],
-    apollo: {
-        autoImports: true,
-        clients: {
-            default: {
-                httpEndpoint: "https://east.cesmii.net/graphql",
-                authHeader: "Authorization",
-                authType: "Bearer",
-                tokenStorage: "cookie",
-                tokenName: "graphql-token",
-            },
-        },
+    "@nuxt/eslint",
+    "@nuxtjs/apollo",
+  ],
+  apollo: {
+    autoImports: true,
+    clients: {
+      default: {
+        httpEndpoint: "https://east.cesmii.net/graphql",
+        authHeader: "Authorization",
+        authType: "Bearer",
+        tokenStorage: "cookie",
+        tokenName: "graphql-token",
+      },
     },
-    eslint: {
-        config: {
-            stylistic: true,
-        },
+  },
+  eslint: {
+    config: {
+      stylistic: true,
     },
-    vite: {
-        vue: {
-            template: {
-                transformAssetUrls,
-            },
-        },
+  },
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
     },
+  },
 });
