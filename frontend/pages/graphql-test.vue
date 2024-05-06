ae2e7a93c3a392448bb3e2a9140cb3f8e326ac98
Scott Fasone <scott181182@yahoo.com>
2024-05-05 20:19:21 -0400

    test(frontend): updated the graphql test page to work with the new login page

--- frontend/pages/graphql-test.vue
+++ frontend/pages/graphql-test.vue
@@ -1,44 +1,22 @@
 <script setup lang="ts">
-import { onBeforeMount } from "vue";
-
 import { useEquipmentWithOEE } from "~/lib/equipment";
 
 
 
-const { onLogin } = useApollo();
-
 const eqRef = useEquipmentWithOEE();
-
-
-
-onBeforeMount(() => {
-  const token = import.meta.env.VITE_APP_GRAPHQL_TOKEN;
-  console.log(`Setting token to ${token}`);
-  onLogin(token)
-    .then(() => console.log("token set"));
-});
 </script>
 
 <template>
   <section class="ma-4">
     <h1>GraphQL Test Page</h1>
     <p>
-      In order for this page to work, you'll have to request a header token from your GraphQL server and set an environment variable to it.
+      In order for this page to work, you'll have to <a href="/login">login</a>.
     </p>
     <p>
-      Specifically, if using the CESMII East instance, go to <a href="https://east.cesmii.net/">https://east.cesmii.net/</a>, open the dropdown for "Developer" and select "Request Header Token".
+      Specifically, if using the CESMII East instance, go to <a href="https://east.cesmii.net/">https://east.cesmii.net/</a>, open the dropdown for "Developer" and select "Authentication Management" to generate a new authenticator to login with.
     </p>
     <p>
-      Then create a <code>.env</code> file in the <code>frontend</code> directory and add the following line:
-    </p>
-    <pre class="mt-4">
-    VITE_APP_GRAPHQL_TOKEN=&lt;token value, excluding "Bearer"&gt;
-        </pre>
-    <p>
-      This token expires relatively quickly, so you might have to update this if you're testing this page for a bit.
-    </p>
-    <p>
-      You should then see the content below:
+      You should then be able to navigate back to this page and see the content below:
     </p>
     <v-list v-if="eqRef" class="border-lg mt-4">
       <v-list-item
