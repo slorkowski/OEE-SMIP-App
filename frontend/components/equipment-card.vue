b290e3f507bf9942d857078ca71482683286f52a
Scott Fasone <scott181182@yahoo.com>
2024-05-10 11:52:16 -0400

    refactor(frontend): added metric clamping and made percentages show the first point

--- frontend/components/equipment-card.vue
+++ frontend/components/equipment-card.vue
@@ -5,15 +5,15 @@
         {{ equipment.name }}
       </h2>
       <v-progress-circular
-        :model-value="equipment.oee"
+        :model-value="oeeSummary.displayValue"
         size="150"
         width="20"
-        :color="getColorState(equipment.oee)"
+        :color="getColorState(oeeSummary.value)"
         class="text-h6 text-center mt-2"
       >
         <div>
-          <h3 class="text-subtitle-1 font-weight-medium">OEE</h3>
-          <span>{{Math.round(equipment.oee)}}%</span>
+          <h3 class="text-subtitle-1 font-weight-medium">{{ oeeSummary.label }}</h3>
+          <span>{{oeeSummary.value.toFixed(1)}}%</span>
         </div>
       </v-progress-circular>
     </v-card-text>
@@ -26,11 +26,11 @@
         flat
         tile
       >
-        <v-sheet class="percent-bg" :color="getColorState(metric.value)" :style="{width: `${metric.value}%`}"/>
+        <v-sheet class="percent-bg" :color="getColorState(metric.value)" :style="{width: `${metric.displayValue}%`}"/>
 
         <v-card-title class="text-subtitle-1 d-flex flex-row justify-space-between align-center h-100" >
           <span class="mr-4">{{ metric.label }}</span>
-          <span>{{metric.value}}%</span>
+          <span>{{metric.value.toFixed(1)}}%</span>
         </v-card-title>
       </v-card>
     </v-card-text>
@@ -38,6 +38,10 @@
 </template>
 
 <script setup lang="ts">
+import { clamp } from "remeda";
+
+
+
 export interface MockEquipment {
   name: string;
   id: number;
@@ -51,11 +55,26 @@ interface Props {
   equipment: MockEquipment;
 }
 
+interface Metric {
+  label: string;
+  value: number;
+  displayValue: number;
+}
+function makeMetric(label: string, value: number): Metric {
+  return {
+    label,
+    value,
+    get displayValue() { return clamp(this.value, { min: 0, max: 100 }); },
+  };
+}
+
 const { equipment } = defineProps<Props>();
+
+const oeeSummary = computed(() => makeMetric("OEE", equipment.oee));
 const metrics = computed(() => [
-  { label: "Availability", value: equipment.availability },
-  { label: "Performance", value: equipment.performance },
-  { label: "Quality", value: equipment.quality },
+  makeMetric("Availability", equipment.availability),
+  makeMetric("Performance", equipment.performance),
+  makeMetric("Quality", equipment.quality),
 ]);
 
 </script>
