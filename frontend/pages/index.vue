6bbb4643e24ff9d71449ddebc0e24df4159a77c7
Connor Guarino <connorgdevelopment@gmail.com>
2024-05-22 08:26:53 -0400

    Added v-else for when no data is present

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,15 +1,27 @@
 <template>
   <v-container>
-    <v-row class="ma-0">
-      <v-col cols="12" class="dashboard-grid">
-        <template v-if="equipments">
-          <EquipmentCard
-            v-for="equipment in equipments"
-            :key="equipment.id"
-            :equipment="equipment"
-            @click="navigateTo(`/equipment/${equipment.id}`)"
-          />
-        </template>
+    <v-row class="ma-0 justify-center">
+      <v-col v-if="equipments" cols="12"  class="dashboard-grid">
+        <EquipmentCard
+          v-for="equipment in equipments"
+          :key="equipment.id"
+          :equipment="equipment"
+          @click="navigateTo(`/equipment/${equipment.id}`)"
+        />
+      </v-col>
+
+      <v-col v-else cols="auto">
+        <v-card class="text-center" @click="refresh()">
+          <v-card-title>
+            No Equipment Found
+          </v-card-title>
+          <v-card-subtitle>
+            Click to Refresh
+          </v-card-subtitle>
+          <v-card-text class="text-h4">
+            <v-icon size="x-large" icon="mdi-refresh"/>
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -20,7 +32,7 @@ definePageMeta({
   title: "Dashboard",
 });
 
-const { data: equipments } = await useAsyncEquipmentWithOEE();
+const { data: equipments, refresh } = await useAsyncEquipmentWithOEE();
 </script>
 
 <style>
